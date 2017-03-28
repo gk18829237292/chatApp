06e64ae226c99c8ba3b1aeb71bc538afe11fa42c
ke.gao <vipgaoke@163.com>
2017-03-28 19:16:57 +0800

    add register activity

--- app/src/main/java/com/gk/chatapp/LoginActivity.java
+++ app/src/main/java/com/gk/chatapp/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.gk.chatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+
+    private TextView tv_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView(){
+        tv_register = (TextView) findViewById(R.id.tv_register);
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
